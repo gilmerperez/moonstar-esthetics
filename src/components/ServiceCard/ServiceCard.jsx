8468db28a0cb2eff4bfe10fab707b8508f98bb9f
gilmerperez <gilmer2002@outlook.com>
2025-06-19 20:00:22 -0400

    Added data to get service cards to show up

---
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,28 @@
+import styles from "./ServiceCard.module.css";
+
+function ServiceCard({ service }) {
+  return (
+    <article className={styles.serviceCard}>
+      {/* Service Image */}
+      <div className={styles.serviceImageContainer}>
+        <img src={service.image} alt={service.name} className={styles.serviceImage} />
+      </div>
+
+      {/* Service Content */}
+      <div className={styles.serviceContent}>
+        {/* Service Name */}
+        <h2 className={styles.serviceName}>{service.name}</h2>
+        {/* Service Duration and Price */}
+        <div className={styles.serviceMetaContent}>
+          <span className={styles.serviceDuration}>{service.duration}</span>
+          <span> | </span>
+          <span className={styles.servicePrice}>${service.price}</span>
+        </div>
+        {/* Service Description */}
+        <p className={styles.serviceDescription}>{service.description}</p>
+      </div>
+    </article>
+  );
+}
+
+export default ServiceCard;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
 import styles from "./Services.module.css";
-// import services from "../data/services.json";
+import services from "../data/services.json";
+import ServiceCard from "../components/ServiceCard/ServiceCard";
 
 function Services() {
   // Set Page Title
   useEffect(() => {
-    document.title = "Moonstar Esthetics | Service";
+    document.title = "Moonstar Esthetics | Services";
   }, []);
 
   return (
     <>
       <main>
-        <section className={styles.servicesContainer}>Services</section>
+        <section className={styles.servicesContainer}>
+          {services.map((category) => (
+            <div key={category.category} className={styles.categorySection}>
+              <h1 className={styles.categoryTitle}>{category.category}</h1>
+              <div className={styles.servicesGrid}>
+                {category.services.map((service) => (
+                  <ServiceCard key={service.id} service={service} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </section>
       </main>
     </>
   );
