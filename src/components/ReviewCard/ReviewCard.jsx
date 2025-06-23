63c7c4be647e7e79ee6481d93b25beeca7a1bfcc
gilmerperez <gilmer2002@outlook.com>
2025-06-23 19:21:50 -0400

    Made the same changes for ReviewCard

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -8,14 +8,8 @@ function ReviewCard({ review }) {
     .join("")
     .toUpperCase();
 
-  // Stars display
-  const stars = Array(5)
-    .fill(0)
-    .map((_, i) => (
-      <span key={i} className={i < review.stars ? styles.filledStar : styles.emptyStar}>
-        <i className="fa-regular fa-star fa-xs"></i>
-      </span>
-    ));
+  // Calculate star percentage
+  const starPercentage = (review.stars / 5) * 100;
 
   return (
     <section className={styles.reviewCard}>
@@ -26,7 +20,22 @@ function ReviewCard({ review }) {
         <h3 className={styles.reviewerName}>{review.name}</h3>
         {/* Review Star Amount and Timestamp */}
         <div className={styles.reviewMetaContent}>
-          {stars}
+          <div className={styles.starsContainer}>
+            <div className={styles.starsBackground}>
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <i key={i} className="fa-regular fa-star"></i>
+                ))}
+            </div>
+            <div className={styles.starsForeground} style={{ width: `${starPercentage}%` }}>
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <i key={i} className="fa-solid fa-star"></i>
+                ))}
+            </div>
+          </div>
           <span className={styles.timestamp}>{review.timestamp}</span>
         </div>
         {/* Review Text */}
