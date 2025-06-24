8837d453bc8ab405a6256071e6321984b7cddfde
gilmerperez <gilmer2002@outlook.com>
2025-06-23 20:23:48 -0400

    Added category descriptions and styling for more details

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,31 +4,31 @@ import services from "../data/services.json";
 import ServiceCard from "../components/ServiceCard/ServiceCard";
 
 function Services() {
-  // Set Page Title
   useEffect(() => {
     document.title = "Moonstar Esthetics | Services";
   }, []);
 
   return (
-    <>
-      <main>
-        <section className={styles.servicesContainer}>
-          {services.map((category) => (
-            // All Service Categories
-            <div key={category.category} className={styles.categorySection}>
-              {/* Individual Service Category Title */}
-              <h1 className={styles.categoryTitle}>{category.category}</h1>
-              {/* Service Card */}
-              <section className={styles.serviceCards}>
-                {category.services.map((service) => (
-                  <ServiceCard key={service.id} service={service} />
-                ))}
-              </section>
-            </div>
-          ))}
-        </section>
-      </main>
-    </>
+    <main>
+      <section className={styles.servicesContainer}>
+        {services.map((category) => (
+          <div key={category.category} className={styles.categorySection}>
+            {/* Category Title */}
+            <h1 className={styles.categoryTitle}>{category.category}</h1>
+
+            {/* Category Description */}
+            {category.description && <p className={styles.categoryDescription}>{category.description}</p>}
+
+            {/* Service Cards */}
+            <section className={styles.serviceCards}>
+              {category.services.map((service) => (
+                <ServiceCard key={service.id} service={service} />
+              ))}
+            </section>
+          </div>
+        ))}
+      </section>
+    </main>
   );
 }
 
